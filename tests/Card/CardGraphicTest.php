3143f8a3e5b6b9edd2331f600a8736d3cd301926
Bruffe <gabbe.rahm@gmail.com>
2023-07-25 13:03:21 +0200

    Created tests for CardGraphic-class

---
+++ tests/Card/CardGraphicTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class CardGraphic.
+ */
+class CardGraphicTest extends TestCase
+{
+    /**
+     * Construct object and verify that the object has the expected
+     * properties, using argument.
+     */
+    public function testCreateCardGraphic()
+    {
+        $card = new CardGraphic(0);
+        $this->assertInstanceOf("\App\Card\CardGraphic", $card);
+    }
+
+    /**
+     * Construct object using argument, and verify that the object
+     * returns the expected filename.
+     */
+    public function testGetUrl()
+    {
+        $card = new CardGraphic(0);
+
+        $exp = "ace_of_spades.png";
+        $res = $card->getUrl();
+        $this->assertEquals($exp, $res);
+    }
+}
